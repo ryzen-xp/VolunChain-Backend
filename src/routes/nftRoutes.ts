15685a91c4f149ce8a3b9209cd0a271c59b3f491
jimenezz22 <ljimenezbogantes@gmail.com>
2025-01-26 16:28:05 -0600

    feat: add NFT routes for creating and retrieving NFTs by ID and user ID

---
+++ src/routes/nftRoutes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import NFTController from '../controllers/NFTController';
+import { body } from 'express-validator';
+
+const router = Router();
+
+router.post(
+  '/nfts',
+  [
+    body('userId').isUUID(),
+    body('organizationId').isUUID(),
+    body('description').isString().notEmpty()
+  ],
+  NFTController.createNFT
+);
+
+router.get('/nfts/:id', NFTController.getNFTById);
+router.get('/users/:userId/nfts', NFTController.getNFTsByUserId);
+
+export default router;
