81c59fdeb502143375785ec88e89bd338dadb263
Ryzen XP <iron4540@gmail.com>
2025-01-27 14:51:58 +0530

    feat:Add health endpoint

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,12 +1,17 @@
-import { DataSource } from 'typeorm';
+import { DataSource } from "typeorm";
+import { createClient } from "redis";
 
 const AppDataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
-  entities: ['dist/entities/**/*.js'], 
-  migrations: ['dist/migrations/**/*.js'], 
+  entities: ["dist/entities/**/*.js"],
+  migrations: ["dist/migrations/**/*.js"],
 });
 
-export default AppDataSource;
+const redisClient = createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+
+export { AppDataSource, redisClient };
