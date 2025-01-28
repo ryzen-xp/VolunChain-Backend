09e1d0d07bed5d3366a5d907116d88ac6a4d7c6f
Josue19-08 <josuemarin2009@hotmail.com>
2025-01-27 22:31:34 -0600

    feat: project entity

---
+++ src/routes/ProjectRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import ProjectController from '../controllers/ProjectController';
+
+const router = Router();
+const projectController = new ProjectController();
+
+router.post('/projects', async (req, res) => projectController.createProject(req, res));
+router.get('/projects/:id', async (req, res) => projectController.getProjectById(req, res));
+router.get('/organizations/:organizationId/projects', async (req, res) => projectController.getProjectsByOrganizationId(req, res));
+
+export default router;
