b71f87f602b8c378c6149ba9fceadeb7b2f38af7
MPSxDev <mparedes.digital@gmail.com>
2025-03-04 15:52:13 -0300

    feat: update project routes

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,12 +1,17 @@
-
 import { Router } from "express";
-import ProjectController from "../controllers/ProjectController";
+import ProjectController from "../controllers/Project.controller";
 
 const router = Router();
 const projectController = new ProjectController();
 
-router.post("/projects", async (req, res) => projectController.createProject(req, res));
-router.get("/projects/:id", async (req, res) => projectController.getProjectById(req, res));
-router.get("/organizations/:organizationId/projects", async (req, res) => projectController.getProjectsByOrganizationId(req, res));
+router.post("/projects", async (req, res) =>
+  projectController.createProject(req, res)
+);
+router.get("/projects/:id", async (req, res) =>
+  projectController.getProjectById(req, res)
+);
+router.get("/organizations/:organizationId/projects", async (req, res) =>
+  projectController.getProjectsByOrganizationId(req, res)
+);
 
 export default router;
